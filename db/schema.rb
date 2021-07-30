3c5a3ddabbec3e4b2ff25ae6c73ba862c4d098ea
robotammie <tammie.kahnhauser@gmail.com>
2021-07-30 12:44:26 -0700

    Add Chats resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_30_192656) do
+ActiveRecord::Schema.define(version: 2021_07_30_194320) do
+
+  create_table "chats", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
