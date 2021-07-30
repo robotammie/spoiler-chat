f7d1295b8f702544757152df62dc66b9c31deec3
robotammie <tammie.kahnhauser@gmail.com>
2021-07-30 12:56:53 -0700

    Add .idea to gitignore; link users and chats

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_30_194320) do
+ActiveRecord::Schema.define(version: 2021_07_30_195021) do
 
   create_table "chats", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "chats_users", id: false, force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "chat_id", null: false
+    t.index ["chat_id", "user_id"], name: "index_chats_users_on_chat_id_and_user_id"
+    t.index ["user_id", "chat_id"], name: "index_chats_users_on_user_id_and_chat_id"
   end
 
   create_table "users", force: :cascade do |t|
